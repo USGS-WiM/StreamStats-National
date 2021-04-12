ee13f130d28c9a4c977d842f7b73e39b359bba24
amedenblik <amedenblik@usgs.gov>
2021-04-12 10:31:55 -0400

    add HttpClientModule to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -20,7 +20,8 @@ import { CenterBottomContentComponent } from './center-bottom-content/center-bot
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
