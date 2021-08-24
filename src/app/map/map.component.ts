3df6ce41223798b60d9eb3b24126279d31ee4aa7
Harper Wavra PR <hwavra-pr>
2021-08-24 15:55:44 -0500

    working on #27

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,6 +4,7 @@ import * as L from 'leaflet';
 import { Config } from 'protractor';
 import { ConfigService } from '../shared/config/config.service';
 import { SettingService } from '../shared/services/setting.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-map',
@@ -17,54 +18,117 @@ export class MapComponent implements OnInit {
   public currentLat: number = 41.1;
   public currentLng: number = -98.7;
   public currentZoom: number = 4;
+  public latestDischarge: string = "";
 
-  constructor(private _mapService: MapService, private _configService: ConfigService, private _settingService: SettingService) { 
+  constructor(private _mapService: MapService, private _configService: ConfigService, private _settingService: SettingService, private _http: HttpClient,
+    ) { 
     this.configSettings = this._configService.getConfiguration();
   }
 
   ngOnInit() {
     // Initialize map
-     this._mapService.map = L.map('map', {
-       center: L.latLng(41.1, -98.7),
-       zoom: 4,
-       minZoom: 4,
-     maxZoom: 19,
-       renderer: L.canvas()
-     });
-
-    // Add basemaps
-    this._mapService.map.addLayer(this._mapService.baseMaps[this._mapService.chosenBaseLayer]);
+    this._mapService.map = L.map('map', {
+      center: L.latLng(41.1, -98.7),
+      zoom: 4,
+      minZoom: 4,
+      maxZoom: 19,
+      renderer: L.canvas(),
+      layers: [this._mapService.baseMaps[this._mapService.chosenBaseLayer]]
+    });
 
     // setting local click point variable
     this._mapService.clickPoint.subscribe((point: {}) => {
       this.clickPoint = point;
     })
-    //this.getStreamgages(-97.55, -94.67, 34.78, 35.90);
-
     // On map click, set click point value, for delineation
     this._mapService.map.on("click", (evt: { latlng: { lat: number; lng: number; }; }) => {
-      this._mapService.setClickPoint(evt.latlng)
-      console.log(evt)
+      this._mapService.setClickPoint(evt.latlng);
     }) 
     // On map move, set currnt lat and long
     this._mapService.map.on("mousemove",(evt: { latlng: { lat: number; lng: number; }; }) => {
       this.currentLat = evt.latlng.lat;
       this.currentLng = evt.latlng.lng;
     }) 
-    
     // On map zoom, set current zoom, display gages
-
+    this._mapService.map.on('zoomend',(evt) => {
+      this.currentZoom = evt.target._zoom;
+      if (this.currentZoom >= 8) {
+        var bBox = this._mapService.map.getBounds();
+        var ne = bBox.getNorthEast(); // LatLng of the north-east corner
+        var sw = bBox.getSouthWest(); // LatLng of the south-west corder
+        this.getStreamgages(sw.lng, ne.lng, sw.lat, ne.lat)
+      } else { 
+        if (this.streamgageLayer !== undefined) this._mapService.map.removeLayer(this.streamgageLayer); // Remove old layer
+        
+      }
+    }) 
+    // On map drag, display gages
+    this._mapService.map.on('dragend',() => {
+      if (this.streamgageLayer !== undefined) this._mapService.map.removeLayer(this.streamgageLayer); // Remove old layer
+      if (this.currentZoom >= 8) {
+        var bBox = this._mapService.map.getBounds();
+        var ne = bBox.getNorthEast(); // LatLng of the north-east corner
+        var sw = bBox.getSouthWest(); // LatLng of the south-west corder
+        this.getStreamgages(sw.lng, ne.lng, sw.lat, ne.lat);
+      } 
+    });
   }
 
   public getStreamgages(xmin: number, xmax: number, ymin: number, ymax: number) {
     var url = this.configSettings.GageStatsServices + "/stations/Bounds?xmin="+xmin+"&xmax="+xmax+"&ymin="+ymin+"&ymax="+ymax+"&geojson=true";
-    console.log(url)
     this._settingService.getEntities(url).subscribe(sg => {
       this.streamgageLayer = sg;
-      console.log(this.streamgageLayer)
+      this.addGeoJSON("streamgages", this.streamgageLayer);
     }, error => {
-      console.log('error')
+      console.log(error);
     });
   }
 
+  private addGeoJSON(LayerName: string, feature: any) {
+    if (LayerName == 'streamgages') {
+      var self = this;
+      var MyIcon = L.icon({
+        iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IB2cksfwAAAAlwSFlzAAAOxAAADsQBlSsOGwAAARVJREFUOI3d1L8rhVEYB/APqROD9C66xSBJDH4MLAYLi4XFJKKYlDBSlpvJrpRBstn8G2b/gXoHdTbDs8jgKt38eF138q1T55yn59MznE6XNqfr34CduMYGXtoB7mIeO7j4K9iXUlqLiAFs4gbPfwHrETHR2E/ gGEetguOYQ3fj3JNSWoyIczy2Ap5i6uNFREyhjq3fgssYRccnPZNFUczknO + rggn7GPuiPp1zPsZKVfAQw1 / U3jOCVdz + BNawhMEfwHFs4w7xHVj39jyqZAR7OPsULIpiNuc8ht6K4BAWcIWnZrAj53yC2YrYeyZTSkcRcdAMrjew3 / 5A/RGxWqvVLsuyfPjYfN1YLaUsS80TtiVtB18BHWxAwwk6imsAAAAASUVORK5CYII=',
+        iconSize: [15, 16],
+        iconAnchor: [7.5, 8],
+        popupAnchor: [0, -11],
+      });
+      this.streamgageLayer = L.geoJSON(feature, {
+        onEachFeature: async function (feature, layer) {
+          var siteNo = feature.properties['Code'];
+          var SSgagepage = 'https://streamstatsags.cr.usgs.gov/gagepages/html/' + siteNo + '.htm';
+          var NWISpage = 'http://nwis.waterdata.usgs.gov/nwis/inventory/?site_no=' + siteNo;
+          layer.on('click',() => {
+            self.getWaterServiceData(siteNo);
+            var innerHTML =  feature.properties['Name'] + ' (' + siteNo + ')' + '<hr><strong>Station Type</strong>: ' + 
+            feature.properties.StationType.name + '</br><strong>Discharge, cfs: </strong>' +
+            self.latestDischarge + 
+            '<br><strong>NWIS page: </strong><a href="' + 
+            NWISpage + ' "target="_blank">link</a></br><strong>StreamStats Gage page: </strong><a href="' + 
+            SSgagepage + '" target="_blank">link</a></br>';
+            layer.bindPopup(innerHTML);
+          })
+        },
+        pointToLayer: function(feature, latlng) {
+          return L.marker(latlng,{icon: MyIcon});
+        }
+      }).addTo(this._mapService.map);
+    }
+  }
+
+  public getWaterServiceData(site: number){
+    var url = 'https://waterservices.usgs.gov/nwis/iv/?format=rdb&sites=' + site + '&parameterCd=00060&siteStatus=all'
+    this._http.get(url, {responseType: 'text'}).subscribe(res => {
+      res = res.split('\n').filter(function(line){ 
+        return line.indexOf( "#" ) == -1;
+      }).join('\n');
+      const parsedString = res.split('\n').map((line) => line.split('\t'));
+      if (parsedString[2][4]) {
+        this.latestDischarge = parsedString[2][4] + " @ " + parsedString[2][2];
+      } else this.latestDischarge = "";
+      console.log(this.latestDischarge)
+    }, error => {
+     console.log(error);
+   })
+  }
 }
--- src/app/shared/services/map.service.ts
+++ src/app/shared/services/map.service.ts
@@ -25,7 +25,6 @@ export class MapService {
 
     constructor(private _http: HttpClient, private _configService: ConfigService) {
         this.chosenBaseLayer = 'Topo';
-      
         this.baseMaps = {
             // {s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png
             OpenStreetMap: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -39,19 +38,19 @@ export class MapService {
                 zIndex: 1,
                 attribution:
                     'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL,' +
-                        'Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+                    'Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
             }),
             CartoDB: L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
                 zIndex: 1,
                 attribution:
                     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; ' +
-                        '<a href="https://cartodb.com/attributions">CartoDB</a>'
+                    '<a href="https://cartodb.com/attributions">CartoDB</a>'
             }),
             Satellite: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
                 zIndex: 1,
                 attribution:
                     'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, ' +
-                        'and the GIS User Community'
+                    'and the GIS User Community'
                 // maxZoom: 10
             }),
             Terrain: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
