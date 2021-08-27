c6d7c6f37e2ac1155299fa08430b87a2695b4383
amedenblik <amedenblik@usgs.gov>
2021-08-26 21:59:49 -0400

    add basic loadConfig unit test

--- src/app/shared/config/config.service.spec.ts
+++ src/app/shared/config/config.service.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { ConfigService } from './config.service';
+import { environment } from 'src/environments/environment';
 
 describe('ConfigService', () => {
   let service: ConfigService;
@@ -13,9 +13,16 @@ describe('ConfigService', () => {
       ]
     });
     service = TestBed.inject(ConfigService);
+    spyOn(service, 'loadConfig').and.callThrough();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('loadConfig should be called', () => {
+    service.loadConfig(environment.configFile);
+    expect(service.loadConfig).toHaveBeenCalled();
+  });
+
 });
