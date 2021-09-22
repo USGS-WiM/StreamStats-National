0a0a719c2b887a693db66cda2283fed871ebb333
ltmetz <lmetz@usgs.gov>
2021-09-22 13:47:40 -0500

    working on #68

--- src/app/center-bottom-content/center-bottom-content.component.spec.ts
+++ src/app/center-bottom-content/center-bottom-content.component.spec.ts
@@ -1,11 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { Workflow } from '../shared/interfaces/workflow/workflow';
 import { WorkflowService } from '../shared/services/workflow.service';
 import { WorkflowSelectionComponent } from './components-bottom/report-builder/workflow-selection/workflow-selection.component';
-
 import { CenterBottomContentComponent } from './center-bottom-content.component';
 
 let mockWorkflowService: Partial<WorkflowService>;
@@ -13,14 +11,13 @@ let mockWorkflowService: Partial<WorkflowService>;
 describe('CenterBottomContentComponent', () => {
   let component: CenterBottomContentComponent;
   let fixture: ComponentFixture<CenterBottomContentComponent>;
-  //let appService: AppService;
   let workflowService: WorkflowService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [
         CenterBottomContentComponent,
-        { provide: WorkflowService, useClass: mockWorkflowService }  
+        //{ provide: WorkflowService, useClass: mockWorkflowService }  
       ],
       imports: [
         HttpClientTestingModule
@@ -39,12 +36,14 @@ describe('CenterBottomContentComponent', () => {
     };
     fixture = TestBed.createComponent(CenterBottomContentComponent);
     workflowService = TestBed.inject(WorkflowService)
-    //workflowService = fixture.debugElement.injector.get(WorkflowService)
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // Testing service dependencies 
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
   it('#selectedWorkflow should display a selected workflow values', () => {
     const workflow: Workflow[] = [{
       title: "Delineation",
@@ -58,7 +57,80 @@ describe('CenterBottomContentComponent', () => {
     expect(component.selectedWorkflows[0].title).toEqual(workflow[0].title);
   });
 
-  afterEach(() => {
-    fixture.destroy();
-  })
+  it('#addFormData should take first if path and push formData', () => {
+    expect(component.formDataOutputs.length).toBe(0);
+    const formData = {
+      options:
+        [
+            {
+                text:"Red",
+                selected: false
+            },
+            {
+                text:"Yellow",
+                selected: false
+            },
+            {
+                text:"Blue",
+                selected: false
+            }
+        ]
+    };
+    component.addFormData(formData);
+    expect(component.formDataOutputs.length).toBe(1);
+  });
+
+  it('#addFormData should take else path and find duplicate', () => {
+    const prevFormmData: any = [{
+      title: "Example Workflow",
+      description: "This is an example workflow.",
+      functionality: "Core",
+      icon: "fa fa-sitemap"
+    },{
+      title: "Second Workflow",
+      description: "This is number two",
+      functionality: "Core",
+      icon: "fa fa-sitemap"
+    }];
+    const formData = {
+      title: "Third Workflow",
+      description: "This is an example workflow.",
+      functionality: "Core",
+      icon: "fa fa-sitemap"
+    };
+    component.formDataOutputs = prevFormmData;
+    expect(component.formDataOutputs.length).toBe(2);
+    expect(component.formDataOutputs).not.toContain(formData);
+    component.addFormData(formData);
+    expect(component.formDataOutputs).toContain(formData);
+  });
+
+  it('#addFormData should take else path and and take else path again', () => {
+    const prevFormmData: any = [{
+      title: "Example Workflow",
+      description: "This is an example workflow.",
+      functionality: "Core",
+      icon: "fa fa-sitemap"
+    },{
+      title: "Second Workflow",
+      description: "This is number two",
+      functionality: "Core",
+      icon: "fa fa-sitemap"
+    }];
+    component.formDataOutputs = prevFormmData;
+    expect(component.formDataOutputs.length).toBe(2);
+    const duplicate = {
+      title: "Example Workflow",
+      description: "This is an example workflow.",
+      functionality: "Core",
+      icon: "fa fa-sitemap"
+    };
+    expect(component.formDataOutputs).toContain(duplicate);
+    const index = component.formDataOutputs.indexOf(duplicate);
+    component.formDataOutputs.splice(index, 1, duplicate);
+    component.addFormData(duplicate);
+    expect(component.formDataOutputs).toContain(duplicate);
+    expect(component.formDataOutputs.length).toBe(2);
+  });
+
 });
--- src/app/center-bottom-content/components-bottom/report-builder/workflow/workflow.component.spec.ts
+++ src/app/center-bottom-content/components-bottom/report-builder/workflow/workflow.component.spec.ts
@@ -1,7 +1,50 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { Workflow } from 'src/app/shared/interfaces/workflow/workflow';
 import { WorkflowComponent } from './workflow.component';
+
+let mockWorkflow: Workflow = {
+    title: "Example Workflow",
+    description: "This is an example workflow.",
+    functionality: "Core",
+    icon: "fa fa-sitemap",
+    steps:
+    [
+        {
+            label: "Select Your Favorite Color",
+            name: "selectColor",
+            type: "checkbox",
+            value: "",
+            validators: 
+            {
+                required: true
+            },
+            options:
+            [
+                {
+                    text:"Red",
+                    selected: false
+                },
+                {
+                    text:"Yellow",
+                    selected: false
+                },
+                {
+                    text:"Blue",
+                    selected: false
+                }
+            ]
+        }
+    ],
+    output: [        {
+        value: "",
+        valueType: "geojson",
+        displayType: "map", 
+        complete: false
+    }],
+    isSelected: true
+};
 
 describe('WorkflowComponent', () => {
   let component: WorkflowComponent;
@@ -9,8 +52,12 @@ describe('WorkflowComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        FormBuilder
+      ],
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        ReactiveFormsModule
       ],
       declarations: [ WorkflowComponent ]
     })
@@ -20,12 +67,18 @@ describe('WorkflowComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WorkflowComponent);
     component = fixture.componentInstance;
+    component.workflow = mockWorkflow;
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create form', () => {
+    expect(component.workflowForm.value).toBeTruthy();
+  });
+
 
   // TODO: update to make sure certain elements are loading in the DOM once more finalized. 
   // it('should have <p> with "workflow works!"', () => {
