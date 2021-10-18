152a1f596ea0b894cac5ef20615bf0a25cd41397
Wavra <hwavra@usgs.gov>
2021-10-18 15:15:32 -0500

    WIP #82

--- src/app/sidebar-right/components-right/report/report.component.ts
+++ src/app/sidebar-right/components-right/report/report.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { ConfigService } from 'src/app/shared/config/config.service';
 import { WorkflowService } from 'src/app/shared/services/workflow.service';
 
 @Component({
@@ -10,11 +12,18 @@ export class ReportComponent implements OnInit {
   public workflowData: any;
   public shownFields = ['INCIDENTNAME', 'COMMENTS', 'GISACRES', 'FIRE_YEAR', 'CREATEDATE', 'ACRES', 'AGENCY', 'SOURCE', 'INCIDENT', 'FIRE_ID', 'FIRE_NAME', 'YEAR', 'STARTMONTH', 'STARTDAY', 'FIRE_TYPE'];
 
-  constructor(private _workflowService: WorkflowService) { }
+  constructor(private _workflowService: WorkflowService, private _configService: ConfigService) { }
 
   ngOnInit(): void {
+
     this._workflowService.completedData.subscribe(data => {
       this.workflowData = data;
     });
+  }
+  ngAfterViewInit(){
+    var configSettings = this._configService.getConfiguration();
+
+    var reportMap = L.map('reportMap').setView([41.1, -98.7], 8);
+    L.tileLayer(configSettings.baseLayers[0].url,{ maxZoom: configSettings.baseLayers[0].maxZoom }).addTo(reportMap);
   }
 }
