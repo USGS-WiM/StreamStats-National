411698e6e01566439822aa55a83d37d27ce7a905
Wavra <hwavra@usgs.gov>
2021-11-09 13:48:18 -0600

    added new labels to popup

--- src/app/sidebar-right/components-right/report/report.component.ts
+++ src/app/sidebar-right/components-right/report/report.component.ts
@@ -12,11 +12,10 @@ export class ReportComponent implements OnInit {
   public workflowData: any;
   public shownFields = ['INCIDENTNAME', 'COMMENTS', 'GISACRES', 'FIRE_YEAR', 'CREATEDATE', 'ACRES', 'AGENCY', 'SOURCE', 'INCIDENT', 'FIRE_ID', 'FIRE_NAME', 'YEAR', 'STARTMONTH', 'STARTDAY', 'FIRE_TYPE', 
   'POLY_INCIDENTNAME','POLY_GISACRES', 'POLY_DATECURRENT', 'IRWIN_FIRECAUSE', 'IRWIN_FIRECAUSEGENERAL', 'IRWIN_FIREDISCOVERYDATETIME','IRWIN_FIREOUTDATETIME','IRWIN_UNIQUEFIREIDENTIFIER'];
-public reportMaps = [];
+  public reportMaps = [];
   public marker: L.Marker;
 
   constructor(private _workflowService: WorkflowService, private _configService: ConfigService) { }
-
   ngOnInit(): void {
     this._workflowService.completedData.subscribe(data => {
       this.workflowData = data;
