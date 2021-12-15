036f0a7ea7a112e74825723afb0fbeaa43951886
amedenblik <amedenblik@usgs.gov>
2021-12-14 20:33:36 -0500

    add about and help content , fixes #126

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Config } from './shared/interfaces/config/config';
+import { ConfigService } from './shared/config/config.service';
 import { MapService } from './shared/services/map.service';
 
 // Import USWDS
@@ -12,9 +14,12 @@ const { modal } = USWDS;
 })
 export class AppComponent{
   title = 'StreamStats-National';
+  private configSettings: Config;
+  public version: string;
 
-	constructor(public mapService: MapService) {
-
+	constructor(public mapService: MapService, private _configService: ConfigService) {
+    this.configSettings = this._configService.getConfiguration();
+    this.version = this.configSettings.version;
 	}
 
   	ngOnInit() {
