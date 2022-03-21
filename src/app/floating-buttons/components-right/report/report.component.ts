b2bd6a7764a8a3704e4dfb19c3dcf2b27f00fc0c
Mitch Samuels <samuels.mitch@gmail.com>
2022-03-21 11:01:12 -0500

    Merged latest, linting (in progress)

--- src/app/floating-buttons/components-right/report/report.component.ts
+++ src/app/floating-buttons/components-right/report/report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { create } from 'domain';
 import * as L from 'leaflet';
 import { ConfigService } from 'src/app/shared/config/config.service';
 import { WorkflowService } from 'src/app/shared/services/workflow.service';
@@ -13,18 +14,30 @@ export class ReportComponent implements OnInit {
   public shownFields = ['INCIDENTNAME', 'COMMENTS', 'GISACRES', 'FIRE_YEAR', 'CREATEDATE', 'ACRES', 'AGENCY', 'SOURCE', 'INCIDENT', 'FIRE_ID', 'FIRE_NAME', 'YEAR', 'STARTMONTH', 'STARTDAY', 'FIRE_TYPE', 
   'POLY_INCIDENTNAME','POLY_GISACRES', 'POLY_DATECURRENT', 'IRWIN_FIRECAUSE', 'IRWIN_FIRECAUSEGENERAL', 'IRWIN_FIREDISCOVERYDATETIME','IRWIN_FIREOUTDATETIME','IRWIN_UNIQUEFIREIDENTIFIER'];
   public reportMaps = [];
+  public printMaps = [];
   public marker: L.Marker;
   public showPrintModal: boolean;
+  public reportTitle: string;
+  public reportComments: string;
 
   constructor(private _workflowService: WorkflowService, private _configService: ConfigService) { }
   ngOnInit(): void {
     this._workflowService.completedData.subscribe(data => {
       this.workflowData = data;
     });
+
+	setTimeout(() => {
+		this.createReportMaps();
+	}, 500);
+
   }
 
   ngAfterViewChecked() {
-    var configSettings = this._configService.getConfiguration();
+	}
+
+  public createReportMaps() {
+	  console.log("REPORT MAPS")
+	var configSettings = this._configService.getConfiguration();
     for (var i = 0; i < this.workflowData.length; ++i) {
       if (this.workflowData[i].outputs.layers) {
         if (this.reportMaps[i] == undefined || this.reportMaps[i] == null) {
@@ -43,11 +56,36 @@ export class ReportComponent implements OnInit {
     }
   }
 
+  public createPrintMaps() {
+	setTimeout(() => {
+		console.log("PRINT MAPS")
+	var configSettings = this._configService.getConfiguration();
+    for (var i = 0; i < this.workflowData.length; ++i) {
+      if (this.workflowData[i].outputs.layers) {
+        if (this.printMaps[i] == undefined || this.printMaps[i] == null) {
+          this.printMaps[i] = L.map('printMap' + i).setView([41.1, -98.7], 8);
+          L.tileLayer(configSettings.baseLayers[0].url,{ maxZoom: configSettings.baseLayers[0].maxZoom }).addTo(this.printMaps[i]);
+          this.workflowData[i].outputs.layers.forEach(layer => {
+            layer.addTo(this.printMaps[i]);
+            this.printMaps[i].fitBounds(layer.getBounds());
+            if (this.workflowData[i].outputs.clickPoint) {
+              this.marker = L.marker(this.workflowData[i].outputs.clickPoint);
+              this.printMaps[i].addLayer(this.marker);
+            }
+          });
+        }
+      }
+    }
+	}, 500);
+}
+
   unsortedKeys() {
     return 0;
   }
 
 	public onPrint() {
+		var newstr = document.querySelector("#reportContent").innerHTML;
+		document.querySelector("#printArea").innerHTML = newstr;
 		window.print();
 	}
 }
